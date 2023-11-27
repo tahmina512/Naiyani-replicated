2afb85c8a87706d2035bf3c79c21c48b2d48c9d3
tish001 <tahminatish001@gmail.com>
2023-11-27 17:02:39 +0600

    invisible the buttons-text when scrolls vertically

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -20,12 +20,8 @@ export class CardListComponent implements OnInit {
   product: Leads[] = [];
   isHovered = false;
   hoveredProductIndex: number | null = null;
-  // isScrolledDown: boolean = false;
-  // lastScrollPosition: number = 0;
-  // secondDivPosition: string = 'relative';
-
-  // hasScrolledX: boolean = false;
-  // secondDivPosition: string = 'absolute';
+  isScrolledAtTop: boolean = true;
+  lastScrollPosition: number = 0;
 
   constructor(
     private cardListService: CardListService,
@@ -35,58 +31,37 @@ export class CardListComponent implements OnInit {
     private cdr: ChangeDetectorRef
   ) {}
 
-  isScrolledDown: boolean = false;
-  lastScrollPosition: number = 0;
-
-  @HostListener('scroll', ['$event'])
+  @HostListener('window:scroll', ['$event'])
   onScroll(event: Event): void {
-    const scrollY = (event.target as HTMLElement).scrollTop;
-    this.isScrolledDown = scrollY > this.lastScrollPosition;
-    this.lastScrollPosition = scrollY;
+    const isScrollMenu = (event.target as HTMLElement).classList.contains(
+      'scrollmenu'
+    );
+
+    if (isScrollMenu) {
+      const scrollY = (event.target as HTMLElement).scrollTop;
+
+      this.isScrolledAtTop = scrollY === 0;
+
+      this.cdr.detectChanges();
+    }
   }
   calculateVisibleHeight(): string {
     const windowHeight = window.innerHeight;
-    return `${windowHeight - 5}px`; // Adjust the value as needed
+    return `${windowHeight - 5}px`; 
   }
-  // calculateRightPosition(): string {
-  //     // Demo value: You can replace this with your dynamic calculation
-  //     const demoMargin = 800;
-  //     const windowWidth = window.innerWidth;
 
-  //     // Calculate the position as a percentage of the window width
-  //     const positionPercentage = 100;
-
-  //     // Subtract the demo margin
-  //     const rightPosition = `calc(${positionPercentage}% - ${demoMargin}px)`;
-
-  //     return rightPosition;
-  // }
   @HostListener('window:resize', ['$event'])
   onResize(event: Event): void {
     this.updateHorizontalScrollerHeight();
   }
-  // @HostListener('window:scroll', ['$event'])
-  // onScroll(event: Event): void {
-  //   this.adjustSecondDivPosition();
-  //   this.cdr.detectChanges();
-  // }
 
-  // @HostListener('window:scroll', ['$event'])
-  // onScroll(event: Event): void {
-  //   console.log('Scroll event detected!');
-  //   this.hasScrolledX = true; // Set the flag when scrolling occurs
-  //   this.adjustSecondDivPosition();
-  // }
   ngAfterViewInit(): void {
     this.updateHorizontalScrollerHeight();
-    // this.adjustSecondDivPosition();
-    // this.cdr.detectChanges();
   }
 
   private updateHorizontalScrollerHeight(): void {
     const calculatedHeight = this.calculateVisibleHeight();
 
-    // Defer the change detection check
     setTimeout(() => {
       this.cdr.detectChanges();
     }, 0);
@@ -121,28 +96,12 @@ export class CardListComponent implements OnInit {
               item.Sourcing_URL
             )
         );
-        // console.log('Data in the component:', this.product);
       },
       (error) => {
         console.error('Error in the component:', error);
       }
     );
   }
-  // private adjustSecondDivPosition(): void {
-  //   const scrollmenu = this.elementRef.nativeElement.querySelector(
-  //     '.scrollmenu'
-  //   ) as HTMLElement;
-
-  //   if (!scrollmenu) {
-  //     console.error('Scrollmenu not found!');
-  //     return;
-  //   }
-
-  //   const hasOverflowX = scrollmenu.scrollWidth > scrollmenu.clientWidth;
-  //   console.log(hasOverflowX);
-  //   this.secondDivPosition = hasOverflowX ? 'relative' : 'absolute';
-  //   console.log('object', this.secondDivPosition);
-  // }
 
   onPreviousPage() {
     this.router.navigate(['/database-list']);
